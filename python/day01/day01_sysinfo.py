16c852bbff83a7adeaf7384fddba5e376b554814
stu-rot <18710221740@163.com>
2025-09-15 11:13:30 +0800

    Update day01_sysinfo.py

--- python/day01/day01_sysinfo.py
+++ python/day01/day01_sysinfo.py
@@ -15,3 +15,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# 显示结果：
+[root@test1 day01]# ./day01_sysinfo.py 
+___ 运维脚本___
+操作系统： Linux 3.10.0-1160.119.1.el7.x86_64
+python版本: 3.6.8
+___ 脚本结束 ___
